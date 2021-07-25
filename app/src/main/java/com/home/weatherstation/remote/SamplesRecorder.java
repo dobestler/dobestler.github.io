b75428894df595c659d1602bfb8a0660d4904bc0
dobestler <dbestler@gmail.com>
2021-07-25 20:48:34 +0200

    Not recording samples to google sheets anymore

--- app/src/main/java/com/home/weatherstation/remote/SamplesRecorder.java
+++ app/src/main/java/com/home/weatherstation/remote/SamplesRecorder.java
@@ -12,11 +12,9 @@ public class SamplesRecorder {
 
     private static final String TAG = SamplesRecorder.class.getSimpleName();
 
-    private final SheetsProvider sheetsProvider;
     private final BigQueryProvider bigQueryProvider;
 
     public SamplesRecorder(Context context) {
-        this.sheetsProvider = SheetsProvider.getInstance(context.getApplicationContext());
         this.bigQueryProvider = BigQueryProvider.getInstance(context.getApplicationContext());
     }
 
@@ -68,52 +66,10 @@ public class SamplesRecorder {
                 deviceNo10.hasBatteryLevelCurrent(), deviceNo10.getBatteryLevel(),
                 outside.hasBatteryLevelCurrent(), outside.getBatteryLevel());
 
-
-        MyLog.i(TAG, "Recording TEMPERATURE samples ...");
-        sheetsProvider.insertSamplesWithRetry(
-                SheetsProvider.TEMPERATURE_SPREADSHEET_ID,
-                SheetsProvider.TEMPERATURE_DATA_SHEET_ID,
-                timestampValue.toString(),
-                deviceNo8.hasTempCurrent(), SheetsProvider.DECIMAL_FORMAT.format(deviceNo8.getTemperature()),
-                deviceNo9.hasTempCurrent(), SheetsProvider.DECIMAL_FORMAT.format(deviceNo9.getTemperature()),
-                deviceNo10.hasTempCurrent(), SheetsProvider.DECIMAL_FORMAT.format(deviceNo10.getTemperature()),
-                outside.hasTempCurrent(), SheetsProvider.DECIMAL_FORMAT.format(outside.getTemperature()));
-
-        MyLog.i(TAG, "Recording HUMIDITY samples ...");
-        sheetsProvider.insertSamplesWithRetry(
-                SheetsProvider.HUMIDITY_SPREADSHEET_ID,
-                SheetsProvider.HUMIDITY_DATA_SHEET_ID,
-                timestampValue.toString(),
-                deviceNo8.hasRelativeHumidity(), String.valueOf(deviceNo8.getRelativeHumidity()),
-                deviceNo9.hasRelativeHumidity(), String.valueOf(deviceNo9.getRelativeHumidity()),
-                deviceNo10.hasRelativeHumidity(), String.valueOf(deviceNo10.getRelativeHumidity()),
-                outside.hasRelativeHumidity(), String.valueOf(outside.getRelativeHumidity()));
-
-        MyLog.i(TAG, "Recording PRECIPITATION samples ...");
-        sheetsProvider.insertSamplesWithRetry(
-                SheetsProvider.PRECIPITATION_SPREADSHEET_ID,
-                SheetsProvider.PRECIPITATION_DATA_SHEET_ID,
-                timestampValue.toString(),
-                false, "",
-                false, "",
-                false, "",
-                outside.hasPrecipitation(), String.valueOf(outside.getPrecipitation()));
-
-        MyLog.i(TAG, "Recording BATTERY samples ...");
-        sheetsProvider.insertSamplesWithRetry(
-                SheetsProvider.BATTERY_SPREADSHEET_ID,
-                SheetsProvider.BATTERY_DATA_SHEET_ID,
-                timestampValue.toString(),
-                deviceNo8.hasBatteryLevelCurrent(), String.valueOf(deviceNo8.getBatteryLevel()),
-                deviceNo9.hasBatteryLevelCurrent(), String.valueOf(deviceNo9.getBatteryLevel()),
-                deviceNo10.hasBatteryLevelCurrent(), String.valueOf(deviceNo10.getBatteryLevel()),
-                outside.hasBatteryLevelCurrent(), String.valueOf(outside.getBatteryLevel()));
-
     }
 
     public float queryAvgHumidity() {
         MyLog.i(TAG, "Reading average humidity data ...");
-//        return sheetsProvider.queryAvg(SheetsProvider.HUMIDITY_SPREADSHEET_ID);
         return bigQueryProvider.queryAvg(BigQueryProvider.TABLE_HUMIDITY);
     }
 }
